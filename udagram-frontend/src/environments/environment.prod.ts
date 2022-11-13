947d6b0a11399f7d9c35b602b80fd5412113703a
yakobsolo <yakisolo2009@gmail.com>
2022-11-13 11:55:36 +0300

    changeing frontend image to v2a:

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2767a5765dd446fa86de7c9ed2d5066-530439431.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
